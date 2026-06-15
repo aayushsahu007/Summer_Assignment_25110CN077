f9765586b6d8c7f00fa59a1a682e6b8f05eeb5b9
aayushsahu007 <sahuaayush942@gmail.com>
2026-06-15 21:03:06 +0530

    Day-2 uploaded.

---
+++ Day-2/WAP to find sum of digits of a number.py
@@ -0,0 +1,16 @@
+#CODE
+
+# Input a number
+num = int(input("Enter a number: "))
+
+sum_digits = 0
+
+# Calculate sum of digits
+while num > 0:
+    digit = num % 10
+    sum_digits += digit
+    num = num // 10
+
+print("Sum of digits =", sum_digits)
+
+#END
