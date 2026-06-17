df02fb9199556421faf03e1ef6ae74e0d76583f9
aayushsahu007 <sahuaayush942@gmail.com>
2026-06-17 18:37:55 +0530

    Day-11 uploaded.

---
+++ Day-11/WAP to wreit function to find maximum.py
@@ -0,0 +1,14 @@
+#CODE
+
+def maximum(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Maximum number =", maximum(num1, num2))
+
+#END
